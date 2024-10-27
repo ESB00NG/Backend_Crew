7202252ef226f76c4d714fcf65d52f2551c773bf
ESB00NG <163206893+ESB00NG@users.noreply.github.com>
2024-10-27 21:18:56 +0100

    add vercel

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -56,6 +56,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://mi-app.vercel.app",
     "http://localhost:3001",
     "http://localhost:3002",  # Nuevo puerto de Next.js
 ]
